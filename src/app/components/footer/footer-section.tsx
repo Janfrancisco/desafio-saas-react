ac33c6a932951a6a2ed8fab1d06bf62d54db6205
Janfrancisco <janramalho.100@gmail.com>
2024-11-17 20:15:59 -0400

    fix bugs in dependency

--- src/app/components/footer/footer-section.tsx
+++ src/app/components/footer/footer-section.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="flex flex-col justify-center items-center my-5">
-      {/* <img src="Logo.png" alt="Logotipo da empresa" /> */}
       <Image width={129} height={40} src="/Logo.png" alt="Image logo" />
       <p className="text-xs font-light text-slate-500">
         © 2024 LivroSaaS. Todos os direitos reservados.
