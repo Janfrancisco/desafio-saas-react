dc7eb07f0d1e526b8dba3302b9dd9f083fdd83e8
Janfrancisco <janramalho.100@gmail.com>
2024-11-25 19:10:16 -0400

    fix:scrolling to top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Cover from "@/components/cover/cover-section";
 import CTA from "@/components/cta/cta";
 import Footer from "@/components/footer/footer-section";
-import HowItWorks from "@/components/how-it-works/HowItWoks";
-import { Price } from "@/components/Price";
+import HowItWorks from "@/components/how-it-works/howItWoks";
+import { Price } from "@/components/price";
 
 export default function Home() {
   return (
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,31 +3,19 @@ import { Menu, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import {
-  Drawer,
-  DrawerContent,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "../ui/drawer";
+import dynamic from "next/dynamic";
 
 export default function Header() {
   const [isMobileMenuOpened, setIsMobileOpened] = useState(false);
 
+  // to avoid the scrolling top bug
+  const Drawer = dynamic(() => import("../ui/wrapper-drawer"), {
+    ssr: false,
+  });
+
   function toggleMobileMenu() {
     setIsMobileOpened((state) => !state);
   }
-  // const [count, setCount] = useState(0);
-  /* async function increment(){
-    setCount(a => a +1);
-    console.log('fbsrjs');
-    const t = await login();
-    console.log(t.data);
-    login().then((data)=>{
-      console.log('data ' +data);
-      
-    }); 
-  } */
 
   return (
     <header className="flex py-5 px-11">
@@ -53,59 +41,10 @@ export default function Header() {
         </div>
         <div className="toggle-btn block sm:hidden cursor-pointer">
           <div className="flex justify-center">
-            <Drawer open={isMobileMenuOpened} direction="left">
-              <DrawerTrigger onClick={toggleMobileMenu}>
-                <Menu />
-              </DrawerTrigger>
-              <DrawerContent>
-                <DrawerHeader>
-                  <div className="flex items-center justify-between">
-                    <DrawerTitle>
-                      <Image
-                        width={129}
-                        height={40}
-                        src="/Logo.png"
-                        alt="Image logo"
-                      />
-                    </DrawerTitle>
-                    <X
-                      size={30}
-                      className="cursor-pointer text-zinc-400"
-                      onClick={toggleMobileMenu}
-                    />
-                  </div>
-                </DrawerHeader>
-                <div className="flex flex-col h-full items-center justify-center">
-                  <li className="flex justify-center items-center pb-3">
-                    <Link
-                      onClick={toggleMobileMenu}
-                      className="text-2xl text-zinc-400"
-                      href={"#howItsWorks"}
-                    >
-                      Funcionamento
-                    </Link>
-                  </li>
-                  <li className="flex justify-center items-center pb-3">
-                    <Link
-                      onClick={toggleMobileMenu}
-                      className="text-2xl text-zinc-400"
-                      href={"#price"}
-                    >
-                      Preço
-                    </Link>
-                  </li>
-                  <li className="flex justify-center items-center pb-3">
-                    <a
-                      id="login"
-                      className="text-black hover:text-gray-600 text-2xl"
-                      href="*"
-                    >
-                      Login
-                    </a>
-                  </li>
-                </div>
-              </DrawerContent>
-            </Drawer>
+            <Drawer
+              isMobileMenuOpened={isMobileMenuOpened}
+              toggleMobileMenu={toggleMobileMenu}
+            ></Drawer>
           </div>
         </div>
       </nav>
