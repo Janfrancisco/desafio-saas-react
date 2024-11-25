f45838803a31227aa1a8497223dca708a91e3a63
Janfrancisco <janramalho.100@gmail.com>
2024-11-25 19:27:38 -0400

    fix:scrolling to top after clicking in link_V2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Cover from "@/components/cover/cover-section";
 import CTA from "@/components/cta/cta";
 import Footer from "@/components/footer/footer-section";
 import HowItWorks from "@/components/how-it-works/howItWoks";
-import { Price } from "@/components/price";
+import Price from "@/components/price";
 
 export default function Home() {
   return (
